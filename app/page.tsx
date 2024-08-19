6dc4412599d9b7d8fa78acc51c2ae987344ddbc7
diego95 <diego.cremesini7@gmail.com>
2024-08-19 14:03:21 +0200

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import {Link} from "@nextui-org/link";
+import {button as buttonStyles} from "@nextui-org/theme";
+
+import {siteConfig} from "@/config/site";
+import {title, subtitle} from "@/components/primitives";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Make&nbsp;</h1>
+        <h1 className={title({color: "violet"})}>beautiful&nbsp;</h1>
+        <br />
+        <h1 className={title()}>websites regardless of your design experience.</h1>
+        <h2 className={subtitle({class: "mt-4"})}>Beautiful, fast and modern React UI library.</h2>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+          href={siteConfig.links.docs}
+        >
+          Documentation
+        </Link>
+      </div>
+    </section>
+  );
+}
